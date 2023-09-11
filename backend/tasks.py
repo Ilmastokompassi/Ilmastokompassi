76337e36707e8da3024922b0e96d0efe85385f6a
leinson <hannah.leinson@gmail.com>
2023-09-11 11:41:48 +0300

    coverage invoke tasks created

--- backend/tasks.py
+++ backend/tasks.py
@@ -12,3 +12,11 @@ def dev(ctx, pty=True):
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
