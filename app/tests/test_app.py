b115a454d803525a88ece8d690493ec3616f0390
leinson <hannah.leinson@gmail.com>
2023-09-11 10:49:46 +0300

    first test created and invoke for tests

---
+++ app/tests/test_app.py
@@ -0,0 +1,17 @@
+import unittest
+from flask import Flask
+from ..app import app
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        # Create a test client using the Flask app
+        app.config['TESTING'] = True
+        self.client = app.test_client()
+
+    def test_index_route(self):
+        # Test the index route
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello world!')
+
