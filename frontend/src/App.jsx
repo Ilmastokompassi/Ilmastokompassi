633f9500392ede0fbd0605c769e59389a8ecf83b
desipeli <mr.desipeli@gmail.com>
2023-09-13 11:35:46 +0300

    Added prettier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,23 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+    const [content, setContent] = useState([])
+    useEffect(() => {
+        const getContent = async () => {
+            const res = await axios('http://localhost:5000/apitest')
+            setContent(res)
+            setContent(res.data.content)
+        }
+        getContent()
+    }, [])
 
-  const [content, setContent] = useState([])
-  useEffect(() => {
-    const getContent = async() => {
-      const res = await axios("http://localhost:5000/apitest")
-      setContent(res)
-      setContent(res.data.content)
-    }
-    getContent()
-  }, [])
-
-  return (
-    <>
-      <p>{content}</p>
-    </>
-  )
+    return (
+        <>
+            <p>{content}</p>
+        </>
+    )
 }
 
 export default App
