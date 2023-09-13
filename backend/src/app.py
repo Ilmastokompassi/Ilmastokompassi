a3b1eef30bbb9fd6574ac431718025d9aa3713a4
desipeli <mr.desipeli@gmail.com>
2023-09-13 10:44:13 +0300

    frontend backend communication

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 import routes  # pylint: disable=unused-import, wrong-import-position, cyclic-import # nopep8
--- backend/src/routes.py
+++ backend/src/routes.py
@@ -4,3 +4,12 @@ from app import app
 @app.route("/")
 def index():
     return "Hello world!"
+
+
+@app.route("/apitest")
+def apitest():
+    res = {
+        "content": "This is the content"
+    }
+
+    return res
