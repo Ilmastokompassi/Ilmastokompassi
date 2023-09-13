983c4ccfa38445852a73b437611df5b3d5f68e68
Desipeli <93584995+Desipeli@users.noreply.github.com>
2023-09-13 10:49:11 +0300

    Add frontend backend communication (#69)
    Closes #52

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [content, setContent] = useState([])
+  useEffect(() => {
+    const getContent = async() => {
+      const res = await axios("http://localhost:5000/apitest")
+      setContent(res)
+      setContent(res.data.content)
+    }
+    getContent()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>{content}</p>
     </>
   )
 }
