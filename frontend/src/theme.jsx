4cbfe7be3df4e2e138743b1ba397799f797a03a3
PaulusParssinen <paulus.parssinen@gmail.com>
2023-09-14 17:35:47 +0300

    Change the test route and run prettier

--- frontend/src/theme.jsx
+++ frontend/src/theme.jsx
@@ -1,11 +1,9 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material'
 
-export const theme = createTheme(
-    {
-        palette: {
-            primary: {
-                main: '#1976d2' /* MUI default */
-            }
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976d2' /* MUI default */,
         },
-    }
-)
+    },
+})
