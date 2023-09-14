cf1cf4efb82c32528f89c5a3967c5ad2e2531ed8
PaulusParssinen <paulus.parssinen@gmail.com>
2023-09-14 16:12:46 +0300

    Add barebones Material UI ThemeProvider sample

---
+++ frontend/src/theme.jsx
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme(
+    {
+        palette: {
+            primary: {
+                main: '#1976d2' /* MUI default */
+            }
+        },
+    }
+)
