adf768747425cd4d77454c72c076f489a646d32e
desipeli <mr.desipeli@gmail.com>
2023-09-14 11:11:34 +0300

    [skip ci] fixed import typo

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { Layout } from './components/layout'
+import { Layout } from './components/Layout'
 import { LandingPage } from './pages/LandingPage'
 import { MaterialPage } from './pages/MaterialPage'
 
