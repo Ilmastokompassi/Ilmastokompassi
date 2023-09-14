655cb73f8c48d671e2a27c676b73e94a51ca32e8
PaulusParssinen <paulus.parssinen@gmail.com>
2023-09-14 11:31:55 +0300

    Configure Vite to proxy /api requests during development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {
+    defineConfig
+} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    /* Proxy for development */
+    proxy: {
+        '/api': {
+            target: 'http://localhost:5000',
+            changeOrigin: true,
+        }
+    }
 })
