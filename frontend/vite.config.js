1f4c92e4005cd543dd00229aa301f5dcadb79dcb
Paulus Pärssinen <paulus.parssinen@gmail.com>
2023-09-14 14:14:06 +0300

    Add Dockerfile for frontend project (#88)
    * Configure Vite to proxy /api requests during development
    
    * Add Dockerfile for frontend
    
    * Fix development proxy routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import {
+    defineConfig
+} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        /* Proxy for development */
+        proxy: {
+            '/api': {
+                /* https://github.com/nodejs/node/issues/40537 */
+                target: 'http://127.0.0.1:5000',
+                changeOrigin: true,
+            }
+        }
+    }
 })
