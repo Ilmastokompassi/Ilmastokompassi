1bf5ed568e22d12f4ae16f3729e82387f5d74ac3
desipeli <mr.desipeli@gmail.com>
2023-09-18 11:26:33 +0300

    [skip ci] template for survey starting page

--- frontend/src/pages/SurveyPage.jsx
+++ frontend/src/pages/SurveyPage.jsx
@@ -1,9 +1,56 @@
-import Survey from '../components/Survey'
+import { Typography, Button, Stack } from '@mui/material'
+import { NavLink } from 'react-router-dom'
+import styled from '@emotion/styled'
+
+const SurveyOptionButton = styled(Button)`
+    /* Add your styling here */
+    /* Example: */
+
+    /* You can add any CSS properties you need */
+`
+
+const SurveyOptions = styled.div`
+    margin: 50px;
+`
+
+const SurveyOptionContainer = styled.div`
+    display: flex; /* Use flexbox layout */
+    flex-direction: column; /* Stack elements vertically */
+    align-items: center; /* Center items horizontally */
+    height: 100vh; /* Make the container full viewport height */
+    margin-top: 100px;
+`
+
+const Heading = styled(Typography)`
+    font-size: 50px;
+`
 
 export function SurveyPage() {
     return (
-        <>
-            <Survey />
-        </>
+        <SurveyOptionContainer>
+            <Heading variant="h1">Miten haluat tehdä kyselyn?</Heading>
+            <SurveyOptions>
+                <Stack spacing={2}>
+                    <SurveyOptionButton
+                        component={NavLink}
+                        to="/"
+                        variant="contained"
+                    >
+                        <Typography className="survey-option">
+                            Tee kysely yksin :(
+                        </Typography>
+                    </SurveyOptionButton>
+                    <SurveyOptionButton
+                        component={NavLink}
+                        to="/"
+                        variant="contained"
+                    >
+                        <Typography className="survey-option">
+                            Tee kysely Ryhmässä :)
+                        </Typography>
+                    </SurveyOptionButton>
+                </Stack>
+            </SurveyOptions>
+        </SurveyOptionContainer>
     )
 }
