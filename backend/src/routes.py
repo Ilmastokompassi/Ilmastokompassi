dbb4eecccc9d7db3eae91b3483995f60990d8159
Ville Saastamoinen <trickwide@gmail.com>
2023-09-19 11:22:41 +0300

    Survey questions by id from json file

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -1,4 +1,7 @@
+from flask import jsonify, abort
+import json
 from app import app
+import os
 
 
 @app.route("/")
@@ -13,3 +16,33 @@ def apitest():
     }
 
     return res
+
+
+def load_json():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    questions_file_path = os.path.join(
+        dir_path, 'static', 'questions.json')
+    try:
+        with open(questions_file_path, 'r') as file:
+            questions = json.load(file)
+    except FileNotFoundError:
+        abort(500, description="questions.json not found")
+
+    return questions
+
+
+@app.route("/api/question")
+def total_questions():
+    return load_json()
+
+
+@app.route("/api/question/<int:id>")
+def question(id):
+    questions = load_json()
+
+    question = next((q for q in questions if q['id'] == id), None)
+
+    if question is None:
+        abort(404, description="Question not found")
+
+    return jsonify(question)
