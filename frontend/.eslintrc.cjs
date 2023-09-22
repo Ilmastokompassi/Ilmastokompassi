3160b3f980fe826d9b59e80b2b41047bab9481bd
hartonenolli <hartonenolli@gmail.com>
2023-09-22 11:35:36 +0300

    Lint ok with cypress

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -7,11 +7,12 @@ module.exports = {
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
         'prettier',
+        'plugin:cypress/recommended',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
     settings: { react: { version: '18.2' } },
-    plugins: ['react-refresh'],
+    plugins: ['react-refresh', 'cypress'],
     rules: {
         'react-refresh/only-export-components': [
             'warn',
--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
+/* eslint-disable */
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
-});
+})
+/* eslint-enable */
