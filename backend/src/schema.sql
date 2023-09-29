f88ca511a88f657e74db1a0497c809bddeed8df9
leinson <hannah.leinson@gmail.com>
2023-09-29 10:55:24 +0300

    sql schema to right format to work in postgres

--- backend/src/schema.sql
+++ backend/src/schema.sql
@@ -2,6 +2,18 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY
 );
 
+CREATE TABLE climate_profiles (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT
+);
+
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    climate_profile_id INTEGER REFERENCES climate_profiles(id)
+);
+
 CREATE TABLE answers (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
@@ -9,14 +21,5 @@ CREATE TABLE answers (
     score INTEGER
 );
 
-CREATE TABLE questions (
-    id SERIAL PRIMARY KEY,
-    content TEXT,
-    climate_profile_id INTEGER REFERENCES climate_profiles(id),
-);
 
-CREATE TABLE climate_profiles (
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    description TEXT
-);
+
