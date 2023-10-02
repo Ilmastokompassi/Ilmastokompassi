053f3d6b59bdd279791dce6418de8df6913ad484
leinson <94794324+leinson@users.noreply.github.com>
2023-10-02 15:20:03 +0300

    added all questions to questions.sql file. show total amount of questions on question cards (#177)

--- backend/src/questions.sql
+++ backend/src/questions.sql
@@ -3,5 +3,37 @@ VALUES
   (1, 'Haluan hankkia enemmän tieteellistä tietoa ilmastonmuutoksesta ', 1),
   (2, 'Haluan puhua, kirjoittaa tai taiteen keinoin kertoa ilmastonmuutoksesta', 2),
   (3, 'Olen kiinnostunut ilmastonmuutoksen vaikutuksista ihmisten hyvinvointiin', 3),
-  (4, 'Olen kiinnostunut ilmastonmuutoksen vaikutuksista omaan elintasooni', 4);
+  (4, 'Olen kiinnostunut ilmastonmuutoksen vaikutuksista omaan elintasooni', 4),
+  (5, 'Haluan tietää enemmän ilmastonmuutoksen syistä ja seurauksista', 1),
+  (6, 'Olen kiinnostunut ilmastonmuutoksen vaikutuksista kulttuuriin ', 2),
+  (7, 'Minulla on suurempi velvollisuus muuttaa elintapojani kuin heikommassa asemassa olevilla ihmisillä', 3),
+  (8, 'Minulle ilmastovastuullinen toiminta liittyy pääosin henkilökohtaisiin valintoihini', 4),
+  (9, 'Haluan opiskella ilmastonmuutosaiheita tulevissa opinnoissa', 1),
+  (10, 'Olen kiinnostunut ilmastonmuutoksen vaikutuksista yhteiskuntaan', 2),
+  (11, 'Haluan muuttaa elintapojani (esim. asuminen, ruokavalio, autoilun ja kuluttamisen vähentäminen) minimoidakseni ilmastovaikutukset', 3),
+  (12, 'Olen valmis maksamaan enemmän tavaroista ja palveluista, jos ne ovat ilmastoystävällisiä', 4),
+  (13, 'Haluan oppia lisää ilmastomuutoksesta fysikaalisena ilmiönä', 1),
+  (14, 'Olen kiinnostunut ilmasto-oikeudenmukaisuudesta', 2),
+  (15, 'Omilla teoilla vaikuttaminen ilmaston puolesta on innostavaa', 3),
+  (16, 'Ostaessani jotain uutta, olen kiinnostunut valitsemaan ilmastoystävällisen vaihtoehdon, jos sellainen on tarjolla', 4),
+  (17, 'Olen kiinnostunut teknologisista innovaatioista, joilla ilmastonmuutosta voitaisiin hillitä', 1),
+  (18, 'Olen kiinnostunut ilmastopolitiikasta', 2),
+  (19, 'Haluan toimia yhdessä muiden kanssa ilmastonmuutoksen hillitsemiseksi', 3),
+  (20, 'Olen valmis lahjoittamaan rahaa ilmastotoimien puolesta', 4),
+  (21, 'Olen kiinnostunut ilmastonmuutoksen vaikutuksista ympäristöön ja maapallon eri alueisiin', 1),
+  (22, 'Haluan vaikuttaa päätöksentekoon ilmastoasiaoissa', 2),
+  (23, 'Haluan kannustaa muita toimimaan ilmastoystävällisesti', 3),
+  (24, 'Haluan kierrättää, silloin kun se on mahdollista', 4),
+  (25, 'Olen valmis osallistumaan ilmastomielenosoituksiin', 2),
+  (26, 'Haluan toimia esimerkkinä muille siitä, kuinka toimia ympäristöystävällisellä tavalla', 3),
+  (27, 'Kuljen mielelläni lyhyet matkat autoilun sijaan pyörällä tai kävellen', 4),
+  (28, 'Haluan keskustella erilaisista näkökulmista ilmastonmuutokseen liittyen, vaikka ne eroaisivat omistani', 2),
+  (29, 'Uskon, että ihmiskunta pystyy sopeutumaan ilmastonmuutoksen vaikutuksiin', 4),
+  (30, 'Mielestäni suurimpia esteitä ilmastonmuutoksen hillinnälle on, että ihmisillä ei ole riittävästi tietoa ja ymmärrystä ilmastonmuutoksesta ja sen vaikutuksista', 1),
+  (31, 'Mielestäni suurimpia esteitä ilmastonmuutoksen hillinnälle on, että päättäjätasolla ei ole tehty tarpeeksi rohkeita ilmastotoimia', 2),
+  (32, 'Mielestäni suurimpia esteitä ilmastonmuutoksen hillinnälle on, että ihmiset eivät ole valmiita muuttamaan elintapojaan merkittävästi', 3),
+  (33, 'Mielestäni suurimpia esteitä ilmastonmuutoksen hillinnälle on, että teknologiseen kehitykseen ei ole panostettu tarpeeksi', 1);
+  
+
+
 
