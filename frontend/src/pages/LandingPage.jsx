163410c38fe009703b3b838d63dcf1d1136d1568
Aarni <aarni.miikkulainen@helsinki.fi>
2023-10-05 10:28:09 +0300

    remove unused content

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -4,7 +4,6 @@ import useSWR from 'swr'
 
 export const LandingPage = () => {
     const { data } = useSWR('/api/test-content')
-    const content = data?.content
 
     useEffect(() => {
         document.title = 'Ilmastokompassi'
