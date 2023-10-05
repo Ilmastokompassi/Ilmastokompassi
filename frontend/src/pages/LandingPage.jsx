55d7924ebdf58ad20da823984a1f502a0d07e3fa
Aarni <aarni.miikkulainen@helsinki.fi>
2023-10-05 10:26:23 +0300

    create rudimentary landing page and change mui theme colour to a green one

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { Typography, Container, Button, Stack } from '@mui/material'
 import useSWR from 'swr'
 
 export const LandingPage = () => {
@@ -8,10 +9,30 @@ export const LandingPage = () => {
     useEffect(() => {
         document.title = 'Ilmastokompassi'
     }, [])
-
     return (
-        <>
-            <p>{content}</p>
-        </>
+        <Container
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '80vh',
+                overflow: 'hidden',
+            }}
+        >
+            <Stack direction="column" alignItems="center">
+                <Typography variant="h2" align="center" paddingBottom={'80px'}>
+                    Tervetuloa!
+                </Typography>
+                <Button
+                    style={{ width: 250, height: 60, fontSize: '20px' }}
+                    variant="contained"
+                    color="primary"
+                    aria-label="move to survey"
+                    href={`/survey`}
+                >
+                    Kyselyyn!
+                </Button>
+            </Stack>
+        </Container>
     )
 }
