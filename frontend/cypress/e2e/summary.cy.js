11392f1f6aa7a57e95976280075f899f2a78b9d6
Ville Saastamoinen <trickwide@gmail.com>
2023-10-06 11:14:02 +0300

    Cypress tests for summary page

---
+++ frontend/cypress/e2e/summary.cy.js
@@ -0,0 +1,18 @@
+describe('From summary page large', function () {
+    beforeEach(function () {
+        cy.visit('/summary/1')
+        cy.viewport(900, 1000)
+    })
+
+    it('title', function () {
+        cy.title().should('eq', 'Oma ilmastoprofiili')
+    })
+
+    it('Oma ilmastoprofiilisi', function () {
+        cy.contains('Oma ilmastoprofiilisi')
+    })
+
+    it('ilmastoasiantuntija', function () {
+        cy.contains('Ilmastoasiantuntija')
+    })
+})
