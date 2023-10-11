70e664fa0ba399fb9faad2bf86acf503ea666cb1
leinson <94794324+leinson@users.noreply.github.com>
2023-10-11 14:43:53 +0300

    group option saves token in frontend and moves to survey (#250)

--- frontend/src/pages/SummaryPage.jsx
+++ frontend/src/pages/SummaryPage.jsx
@@ -9,6 +9,7 @@ import {
     Card,
     CardContent,
     CardHeader,
+    Button,
 } from '@mui/material'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
@@ -29,7 +30,7 @@ export const SummaryPage = () => {
 
     return (
         <Container>
-            <Stack spacing={3} paddingBottom={'50px'}>
+            <Stack spacing={3} paddingBottom={'50px'} alignItems={'center'}>
                 <Typography
                     variant="h1"
                     sx={{
@@ -44,84 +45,103 @@ export const SummaryPage = () => {
                 >
                     Oma ilmastoprofiilisi
                 </Typography>
-                <Typography
-                    variant="h2"
-                    sx={{
-                        fontSize: {
-                            xs: '1.5em',
-                            sm: '1.75em',
-                            md: '2em',
-                        },
-                        textAlign: 'center',
-                        p: '10px',
-                    }}
-                >
-                    Olet vastannut {answerCount}/{totalQuestions} kysymykseen ja
-                    alta löydät oman ilmastoprofiilisi!
-                </Typography>
 
-                <Box
-                    sx={{
-                        display: { xs: 'flex', md: 'flex' },
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        paddingBottom: '40px',
-                    }}
-                >
-                    <Box paddingBottom={'20px'} padding={'10px'}>
-                        <Skeleton
-                            variant="rectangular"
-                            width={250}
-                            height={250}
-                        />
-                    </Box>
-                </Box>
-                <Card variant="outlined" sx={{ width: '100%' }}>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            overflowWrap: 'break-word',
-                        }}
-                    >
-                        <CardHeader
-                            sx={{ textAlign: 'center' }}
-                            titleTypographyProps={{
-                                variant: 'h2',
+                {answerCount > 0 ? (
+                    <>
+                        <Typography
+                            variant="h2"
+                            sx={{
                                 fontSize: {
                                     xs: '1.5em',
                                     sm: '1.75em',
                                     md: '2em',
                                 },
+                                textAlign: 'center',
+                                p: '10px',
                             }}
-                            title="Ilmastoasiantuntija"
-                        ></CardHeader>
-                        <CardContent sx={{ flex: '1 0 auto' }}>
-                            <Typography
-                                variant="h3"
+                        >
+                            Olet vastannut {answerCount}/{totalQuestions}{' '}
+                            kysymykseen ja alta löydät oman ilmastoprofiilisi!
+                        </Typography>
+
+                        <Box
+                            sx={{
+                                display: { xs: 'flex', md: 'flex' },
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                paddingBottom: '40px',
+                            }}
+                        >
+                            <Box paddingBottom={'20px'} padding={'10px'}>
+                                <Skeleton
+                                    variant="rectangular"
+                                    width={250}
+                                    height={250}
+                                />
+                            </Box>
+                        </Box>
+                        <Card variant="outlined" sx={{ width: '100%' }}>
+                            <Box
                                 sx={{
-                                    fontSize: {
-                                        xs: '0.9em',
-                                        sm: '1em',
-                                        md: '1.1em',
-                                    },
-                                    p: '10px',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    overflowWrap: 'break-word',
                                 }}
                             >
-                                Sinussa on potentiaalia ilmastoasiantuntijaksi.
-                                Ilmastoasiantuntijana olet kiinnostunut
-                                ilmastonmuutoksen tieteellisestä puolesta.
-                                Janoat saada tietää, kuinka ilmasto ja
-                                yhteiskunta toimivat yhdessä. Koet tieteeseen
-                                pohjautuvan päätöksenteon tärkeäksi ja näet
-                                uusien innovaatioiden välttämättömyyden
-                                matkallamme kohti planetaarisia rajoja
-                                kunnioittavaa elämää.
-                            </Typography>
-                        </CardContent>
-                    </Box>
-                </Card>
+                                <CardHeader
+                                    sx={{ textAlign: 'center' }}
+                                    titleTypographyProps={{
+                                        variant: 'h2',
+                                        fontSize: {
+                                            xs: '1.5em',
+                                            sm: '1.75em',
+                                            md: '2em',
+                                        },
+                                    }}
+                                    title="Ilmastoasiantuntija"
+                                ></CardHeader>
+                                <CardContent sx={{ flex: '1 0 auto' }}>
+                                    <Typography
+                                        variant="h3"
+                                        sx={{
+                                            fontSize: {
+                                                xs: '0.9em',
+                                                sm: '1em',
+                                                md: '1.1em',
+                                            },
+                                            p: '10px',
+                                        }}
+                                    >
+                                        Sinussa on potentiaalia
+                                        ilmastoasiantuntijaksi.
+                                        Ilmastoasiantuntijana olet kiinnostunut
+                                        ilmastonmuutoksen tieteellisestä
+                                        puolesta. Janoat saada tietää, kuinka
+                                        ilmasto ja yhteiskunta toimivat yhdessä.
+                                        Koet tieteeseen pohjautuvan
+                                        päätöksenteon tärkeäksi ja näet uusien
+                                        innovaatioiden välttämättömyyden
+                                        matkallamme kohti planetaarisia rajoja
+                                        kunnioittavaa elämää.
+                                    </Typography>
+                                </CardContent>
+                            </Box>
+                        </Card>
+                    </>
+                ) : (
+                    <>
+                        <Typography>Et ole vielä vastannut kyselyyn</Typography>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            aria-label="move to survey"
+                            href={`/survey`}
+                        >
+                            Siirry tästä kyselyyn!
+                        </Button>
+                    </>
+                )}
             </Stack>
         </Container>
     )
