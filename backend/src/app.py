13377cac2b6a0c29c14b2b647d77b2a8cca14fef
Paulus Pärssinen <paulus.parssinen@gmail.com>
2023-10-11 15:18:46 +0300

    Use blueprints for the routing

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,29 @@
-from . import create_app
-from .config import EnvironmentConfig
+from flask import Flask
+from flask_cors import CORS
+from src.extensions import db
+from src.config import EnvironmentConfig
+from src.api import api
 
-app = create_app(EnvironmentConfig)
+
+# Create the Flask app using the app factory pattern
+def create_app(config=EnvironmentConfig):
+    _app = Flask(__name__)
+
+    # Enable CORS
+    CORS(_app)
+
+    # Apply configuration
+    _app.config.from_object(config)
+
+    # Initialize extensions
+    db.init_app(_app)
+
+    # Import routes
+    _app.register_blueprint(api)
+
+    return _app
+
 
 if __name__ == "__main__":
+    app = create_app(EnvironmentConfig)
     app.run(host="0.0.0.0")
