7c454040ee66985a91ee7ff850a2c1effb47712e
PaulusParssinen <paulus.parssinen@gmail.com>
2023-10-20 17:00:40 +0300

    Fix static asset handling in jest
    See https://jestjs.io/docs/webpack#handling-static-assets

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
-import { Background} from '../components/Background'
+import { Background } from '../components/Background'
 
 export const Layout = () => (
     <>
