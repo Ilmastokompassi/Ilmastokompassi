b75d4fd3bbbeffde90c2b64ff53683a5de108eea
hartonenolli <hartonenolli@gmail.com>
2023-11-06 10:47:11 +0200

    Cypress tests for multiple profiles

--- frontend/cypress/e2e/survey.spec.cy.js
+++ frontend/cypress/e2e/survey.spec.cy.js
@@ -22,4 +22,47 @@ describe('From survey page large ', function () {
             'Maailma muuttuu ja sinussa on ainesta johtamaan tätä muutosta.'
         )
     })
+
+    it('Move from survey page, answer four questions "Täysin samaa mieltä" and then move to summary page', function () {
+        cy.title().should('eq', 'Kyselyt')
+        cy.get('#btn-survey-alone').click()
+        cy.title().should('eq', 'Ilmastoprofiili - Kysymys 1.')
+        for (let i = 0; i < 4; i++) {
+            // eslint-disable-next-line
+            cy.wait(500)
+            cy.contains('Täysin samaa mieltä').click()
+        }
+        cy.visit('/kysymys/33')
+        cy.contains('Lopeta kysely').click()
+        cy.title().should('eq', 'Ilmastoprofiili - Tulokset')
+        cy.contains('4/33')
+        cy.contains('Ilmastoasiantuntija')
+        cy.contains('Mielipidevaikuttaja')
+        cy.contains('Kestävän elämäntavan etsijä')
+        cy.contains('Eettinen kuluttaja')
+    })
+
+    it('Make survey 5 times in a group and check that the results are correct', function () {
+        cy.title().should('eq', 'Kyselyt')
+        cy.createGroupWithToken('TOKEN', 'Ryhmä luotu onnistuneesti!')
+        cy.joinGroupWithToken('TOKEN')
+        for (let i = 0; i < 4; i++) {
+            cy.title().should('eq', 'Ilmastoprofiili - Kysymys 1.')
+            cy.contains('Täysin samaa mieltä').click()
+            // eslint-disable-next-line
+            cy.wait(500)
+            cy.visit('/kysymys/33')
+            cy.contains('Lopeta kysely').click()
+            cy.title().should('eq', 'Ilmastoprofiili - Tulokset')
+            cy.visit('/kyselyt')
+            cy.joinGroupWithToken('TOKEN')
+        }
+        cy.title().should('eq', 'Ilmastoprofiili - Kysymys 1.')
+        cy.contains('Jokseenkin samaa mieltä').click()
+        cy.visit('/kysymys/33')
+        cy.contains('Lopeta kysely').click()
+        cy.title().should('eq', 'Ilmastoprofiili - Tulokset')
+        cy.contains('1/33')
+        cy.contains('Ryhmäsi TOKEN jakauma')
+    })
 })
