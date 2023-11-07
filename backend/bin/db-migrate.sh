860f067e08bef6c8cefa6a9d0d3327d2b64cc6bf
Paulus Pärssinen <paulus.parssinen@gmail.com>
2023-11-07 15:04:23 +0200

    Make migration script runnable from anywhere

--- backend/bin/db-migrate.sh
+++ backend/bin/db-migrate.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$PARENT_PATH"
+source ../.env
+
 COMMAND="psql"
 ARGUMENTS=("$DATABASE_URI" \
-  -f migrations/01-schema.sql \
-  -f migrations/02-climateprofiles.sql\
-  -f migrations/03-questions.sql"$@")
+  -f ../migrations/01-schema.sql \
+  -f ../migrations/02-climateprofiles.sql\
+  -f ../migrations/03-questions.sql"$@")
 
 echo "> $COMMAND" "$(printf "%q " "${ARGUMENTS[@]}")"
 exec "$COMMAND" "${ARGUMENTS[@]}"
