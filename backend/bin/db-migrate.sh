fafca8edb89b4c5cd2c77d285f88d004da6a552e
desipeli <mr.desipeli@gmail.com>
2023-11-08 11:15:39 +0200

    moved quiz questions to migrations

--- backend/bin/db-migrate.sh
+++ backend/bin/db-migrate.sh
@@ -14,7 +14,8 @@ COMMAND="psql"
 ARGUMENTS=("$DATABASE_URI" \
   -f ../migrations/01-schema.sql \
   -f ../migrations/02-climateprofiles.sql\
-  -f ../migrations/03-questions.sql"$@")
+  -f ../migrations/03-questions.sql\
+  -f ../migrations/04-quiz_questions.sql"$@")
 
 echo "> $COMMAND" "$(printf "%q " "${ARGUMENTS[@]}")"
 exec "$COMMAND" "${ARGUMENTS[@]}"
