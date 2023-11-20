20725899b9600330ddd38e908c454548536204f3
Ville Saastamoinen <trickwide@gmail.com>
2023-11-20 11:22:48 +0200

    Additional Cypress E2E tests for quiz

--- frontend/cypress/e2e/factQuiz.cy.js
+++ frontend/cypress/e2e/factQuiz.cy.js
@@ -54,4 +54,54 @@ describe('From quiz page large ', function () {
 
         cy.title().should('eq', 'Tietovisa - Kysymys 2.')
     })
+
+    it('Move from first question to last question and answer question with answer option', function () {
+        cy.title().should('eq', 'Tietovisa - Kysymys 1.')
+        cy.contains('1/8')
+
+        cy.visit('/tietovisa/8')
+        cy.title().should('eq', 'Tietovisa - Kysymys 8.')
+        cy.contains(
+            '8. Käynnissä olevalla ilmastonmuutoksella on paljon vaikutuksia maapallon eliölajien ja populaatioiden hyvinvointiin. Osaatko sanoa, mitkä seuraavista väitteistä ovat totta ja mitkä tarua?'
+        )
+        cy.contains('8/8')
+
+        cy.contains(
+            'Kaikki eliölajit eivät pysty sopeutumaan nopeasti muuttuviin elinolosuhteisiin'
+        ).click()
+        cy.contains('Vastaa').click()
+        cy.contains('Oikeat vastaukset')
+        cy.contains('Selitykset').click()
+        cy.contains(
+            'Ihmisten toiminnan aiheuttaman ilmastonmuutoksen myötä mm. vuodenaikojen pituudet ja ajankohdat sekä paikalliset sademäärät muuttuvat, mikä muuttaa lajien elinalueita, vuodenkiertoa, yhteisöjen rakenteita ja ekosysteemien toimintaa.'
+        )
+
+        cy.contains('Lopeta kysely')
+    })
+
+    it('Selecting correct answer renders correct checkmark after submitting answer', function () {
+        cy.title().should('eq', 'Tietovisa - Kysymys 1.')
+        cy.contains(
+            '1. Ilmastonmuutokseen liittyviä väitteitä on kaikenlaisia. Tunnistatko mitkä seuraavista ovat totta?'
+        )
+        cy.contains(
+            'Käynnissä oleva kasvihuoneilmiön voimistuminen on seurausta ihmisen toimista'
+        ).click()
+        cy.contains('Vastaa').click()
+        cy.get('.MuiSvgIcon-root.MuiSvgIcon-colorSuccess')
+    })
+
+    it('Selecting incorrect answer renders incorrect checkmark after submitting answer', function () {
+        cy.visit('/tietovisa/2')
+        cy.title().should('eq', 'Tietovisa - Kysymys 2.')
+        cy.contains(
+            '2. Ilmastonmuutos aiheuttaa muutoksia luonnossa ja nämä muutokset puolestaan vaikuttavat ilmastonmuutokseen. Mitä luulet, mitkä seuraavista ilmiöistä voimistavat ilmastonmuutosta?'
+        )
+
+        cy.contains(
+            'Kasvien hiilensidonnan (photosynteesin) lisääntyminen'
+        ).click()
+        cy.contains('Vastaa').click()
+        cy.get('.MuiSvgIcon-root.MuiSvgIcon-colorError')
+    })
 })
