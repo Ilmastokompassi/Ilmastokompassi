7cb00bd4813d011bf15422e4c51c0a95860c4866
PaulusParssinen <paulus.parssinen@gmail.com>
2023-11-22 14:50:05 +0200

    Disable button text capitalization

--- frontend/src/theme.jsx
+++ frontend/src/theme.jsx
@@ -2,6 +2,11 @@ import { createTheme } from '@mui/material'
 import { LinkBehavior } from './components/LinkBehavior'
 
 export const theme = createTheme({
+    typography: {
+        button: {
+            textTransform: 'none',
+        },
+    },
     palette: {
         primary: {
             main: '#27632a',
