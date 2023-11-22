baaaef62160e8d3988dfcaeccc523cbbb305eea5
Paulus Pärssinen <paulus.parssinen@helsinki.fi>
2023-11-22 08:06:38 +0000

    Make the chart.js doughnut responsive (#421)

--- frontend/src/components/SummaryDoughnut.jsx
+++ frontend/src/components/SummaryDoughnut.jsx
@@ -1,39 +1,53 @@
 import PropTypes from 'prop-types'
+import { Box } from '@mui/material'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, Colors } from 'chart.js'
 import { Doughnut } from 'react-chartjs-2'
 
 ChartJS.register(ArcElement, Tooltip, Legend, Colors)
 
 export const SummaryDoughnut = ({ data }) => (
-    <Doughnut
-        data={{
-            labels: data.map((x) => x.label),
-            datasets: [
-                {
-                    label: '%',
-                    data: data.map((x) => x.value),
-                    borderWidth: 1,
-                },
-            ],
+    <Box
+        position="relative"
+        width={{
+            xs: '60vw',
+            sm: '50vw',
+            md: '40vw',
         }}
-        options={{
-            plugins: {
-                legend: {
-                    labels: {
-                        boxHeight: 40,
-                        usePointStyle: true,
-                        font: {
-                            size: 20, // Adjust the font size as needed
+        height={{
+            xs: '60vw',
+            sm: '50vw',
+            md: '40vw',
+        }}
+    >
+        <Doughnut
+            data={{
+                labels: data.map((x) => x.label),
+                datasets: [
+                    {
+                        label: '%',
+                        data: data.map((x) => x.value),
+                        borderWidth: 1,
+                    },
+                ],
+            }}
+            options={{
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        labels: {
+                            usePointStyle: true,
+                            font: {
+                                size: 16, // Adjust the font size as needed
+                            },
+                            color: 'black',
                         },
-                        color: 'black',
                     },
                 },
-            },
-        }}
-    />
+            }}
+        />
+    </Box>
 )
 
-// Validate the data prop
 SummaryDoughnut.propTypes = {
     data: PropTypes.arrayOf(
         PropTypes.shape({
