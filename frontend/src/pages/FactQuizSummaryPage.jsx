34b9dee48058bb0f268f74f0db1bcc2bf540da42
desipeli <mr.desipeli@gmail.com>
2023-11-24 11:24:12 +0200

    removed unused Card

--- frontend/src/pages/FactQuizSummaryPage.jsx
+++ frontend/src/pages/FactQuizSummaryPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, Container, Stack, Typography } from '@mui/material'
+import { Box, Button, Container, Stack, Typography } from '@mui/material'
 import useSWR from 'swr'
 import QuizSummaryAccordion from '../components/QuizSummaryAccordion' // Import the new component
 
