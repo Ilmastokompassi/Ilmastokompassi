df7b49a6b2611118fa9153724d496d5ab01ac92f
desipeli <mr.desipeli@gmail.com>
2023-11-30 13:34:04 +0200

    navbar at the bottom of the page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { Box, Container, Stack, Typography } from '@mui/material'
 
 export const Footer = () => (
-    <Box component="footer" position="static" bottom={0} left={0} width="100%">
+    <Box component="footer" mt="auto">
         <Box sx={{ backgroundColor: 'primary.main' }}>
             <Container>
                 <Stack paddingY={1}>
