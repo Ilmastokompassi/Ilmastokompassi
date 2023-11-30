4f3a89f2e42d60552825a60aeb85f3a6d13b9300
hartonenolli <hartonenolli@gmail.com>
2023-11-30 13:09:18 +0200

    Footer fixes, still needs work on placement

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import { Box, Container, Stack, Typography } from '@mui/material'
 
 export const Footer = () => (
-    <Box component="footer" position="fixed" bottom={0} left={0} width="100%">
+    <Box component="footer" position="static" bottom={0} left={0} width="100%">
         <Box sx={{ backgroundColor: 'primary.main' }}>
             <Container>
-                <Stack paddingY={2}>
-                    <Typography variant="h5" component="div" color="white">
+                <Stack paddingY={1}>
+                    <Typography variant="h6" component="div" color="white">
                         Ilmastokompassi
                     </Typography>
                 </Stack>
