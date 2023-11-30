38a4dc7b6445c4b694318da3e88dfc0976f77a1e
Olli <101706149+hartonenolli@users.noreply.github.com>
2023-11-30 13:46:10 +0200

    Footer fixes, still needs work on placement (#487)
    * Footer fixes, still needs work on placement
    
    * navbar at the bottom of the page
    
    * centered text
    
    ---------
    
    Co-authored-by: desipeli <mr.desipeli@gmail.com>

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,16 @@
 import { Box, Container, Stack, Typography } from '@mui/material'
 
 export const Footer = () => (
-    <Box component="footer" position="fixed" bottom={0} left={0} width="100%">
+    <Box component="footer" mt="auto">
         <Box sx={{ backgroundColor: 'primary.main' }}>
             <Container>
-                <Stack paddingY={2}>
-                    <Typography variant="h5" component="div" color="white">
+                <Stack paddingY={1}>
+                    <Typography
+                        textAlign={'center'}
+                        variant="h6"
+                        component="div"
+                        color="white"
+                    >
                         Ilmastokompassi
                     </Typography>
                 </Stack>
