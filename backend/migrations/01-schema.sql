0a8355ce4ff253899c9db39a6ccc93467bda6000
PaulusParssinen <paulus.parssinen@gmail.com>
2023-12-04 16:05:16 +0200

    Wrap each migration with transaction

--- backend/migrations/01-schema.sql
+++ backend/migrations/01-schema.sql
@@ -1,23 +1,25 @@
+BEGIN;
+
 CREATE TABLE groups (
-    token TEXT PRIMARY KEY
+  token TEXT PRIMARY KEY
 );
 
 CREATE TABLE responses (
-    id SERIAL PRIMARY KEY,
-    group_token TEXT REFERENCES groups(token)
+  id SERIAL PRIMARY KEY,
+  group_token TEXT REFERENCES groups(token)
 );
 
 /* Climate profile survey */
 CREATE TABLE profiles (
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    description TEXT
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  description TEXT
 );
 
 CREATE TABLE profile_questions (
-    id SERIAL PRIMARY KEY,
-    content TEXT,
-    profile_id INTEGER REFERENCES profiles(id)
+  id SERIAL PRIMARY KEY,
+  content TEXT,
+  profile_id INTEGER REFERENCES profiles(id)
 );
 
 CREATE TABLE profile_answers (
@@ -29,22 +31,24 @@ CREATE TABLE profile_answers (
 
 /* Fact quiz */
 CREATE TABLE quiz_questions (
-    id SERIAL PRIMARY KEY,
-    content TEXT,
-    info_text TEXT,
-    introduction TEXT
+  id SERIAL PRIMARY KEY,
+  content TEXT,
+  info_text TEXT,
+  introduction TEXT
 );
 
 CREATE TABLE quiz_question_options (
-    id SERIAL PRIMARY KEY,
-    option TEXT,
-    is_correct BOOLEAN,
-    question_id INTEGER REFERENCES quiz_questions(id)
+  id SERIAL PRIMARY KEY,
+  option TEXT,
+  is_correct BOOLEAN,
+  question_id INTEGER REFERENCES quiz_questions(id)
 );
 
 CREATE TABLE quiz_answers (
-    id SERIAL PRIMARY KEY,
-    response_id INTEGER REFERENCES responses(id),
-    question_id INTEGER REFERENCES quiz_questions(id),
-    selected_option_id INTEGER REFERENCES quiz_question_options(id)
+  id SERIAL PRIMARY KEY,
+  response_id INTEGER REFERENCES responses(id),
+  question_id INTEGER REFERENCES quiz_questions(id),
+  selected_option_id INTEGER REFERENCES quiz_question_options(id)
 );
+
+COMMIT;
