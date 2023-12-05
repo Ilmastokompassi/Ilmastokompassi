af7a04e3f83ee06bd90e2ec0d107c7338234529d
Paulus Pärssinen <paulus.parssinen@helsinki.fi>
2023-12-05 07:14:53 +0000

    Wrap each migration with transaction (#505)

--- backend/migrations/05-add-short-role-description.sql
+++ backend/migrations/05-add-short-role-description.sql
@@ -1,6 +1,11 @@
-ALTER TABLE profiles ADD COLUMN short_description TEXT;
+BEGIN;
+
+ALTER TABLE profiles 
+  ADD COLUMN short_description TEXT;
 
 UPDATE profiles SET short_description = 'Ilmastoasiantuntija-hiiri joka osoittaa tilastoja keltaisella taustalla. Tilastotaululla on myös maapallo, jolla on lippis päässä.' WHERE id = 1;
 UPDATE profiles SET short_description = 'Mielipidevaikuttaja-pupu sinisellä taustalla, jolla on kädessään kyltti missä ilmaistaan "minä rakastan maapalloa". Maapallolla on baskeri päässä.' WHERE id = 2;
 UPDATE profiles SET short_description = 'Kestävän elämäntavan etsijä-ilves oranssilla taustalla, jonka puhekuplassa on maapallo jolla on cowboy-hattu päässä.' WHERE id = 3;
 UPDATE profiles SET short_description = 'Eettinen kuluttaja-kettu vihreällä taustalla, joka osoittaa hänen t-paitaan jossa ilmaistaan "minä rakastan maapalloa". Maapallolla on pipo päässä.' WHERE id = 4;
+
+COMMIT;
